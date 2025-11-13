d564ba961c425f936bde47b7e5fd2e1b594220f3
mohd-sahil-7 <mhdsahil8910@gmail.com>
2025-11-13 09:45:09 +0530

    Add project files.

---
+++ isPrime/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+
+namespace program
+{
+    class firstProject
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            bool isPrime = true;
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+            {
+                Console.WriteLine("Given number is a prime number");
+            }
+            else
+            {
+                Console.WriteLine("Given number is not a prime number");
+            }
+        }
+    }
+
+}
+
